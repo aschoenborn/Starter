c368e5e8221a2d84c44ea94091393d6e4834333e
Unknown <aschoen883@gmail.com>
2018-03-22 21:29:47 -0500

    Second Commit

--- assets/js/firstComp/firstComp.js
+++ assets/js/firstComp/firstComp.js
@@ -5,18 +5,32 @@ class Layout extends Component {
   constructor () {
     super()
     this.state = {
-      name: 'Joe'
+      name: 'Joe',
+      health: 100,
+      level: 1,
+      lowLevelClass: 'danger-red'
     }
+    this.clickedGirl = this.clickedGirl.bind(this)
   }
-  clickedBtn = () => {
-    console.log('swag')
+
+  clickedGirl () {
+    this.setState({
+      health: this.state.health - 25
+    }, function() {
+      console.log('Her health is now: ' + this.state.health)
+    })
   }
   render () {
-    return (
-      <div>
-        <Header />
-        <img src="/img/bape.png" />
+    return (<div id="parent">
+      <div className={`blue-bg ${(this.state.health < 55) ? 
+      this.state.lowLevelClass : ''}`}>
+        <h3>Name: {this.state.name}</h3>
+        <h3>Health: {this.state.health}</h3>
+        <h3>Level: {this.state.level}</h3>
+        <img src="/img/bape.png" alt={'girl with bape'} 
+          onClick={this.clickedGirl}/>
       </div>
+    </div>
     )
   }
 }
